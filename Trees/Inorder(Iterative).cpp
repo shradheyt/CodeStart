80f208c47ad4489c70522c7049f8484f8ec3eae7
shradheyt <shradhey029tripathi@gmail.com>
2016-07-15 23:50:25 +0530

    Iterative Version of InOrder Traversal

---
+++ Trees/Inorder(Iterative).cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+struct TreeNode
+{
+	int data;
+	struct TreeNode* left;
+	struct TreeNode* right;
+} *root=NULL;
+
+//Inorder Interative Traversal
+void inorder(struct TreeNode* root)
+{
+	stack<TreeNode*> s;
+	while(true)
+	{
+		while(root)
+		{
+			s.push(root);
+			root=root->left;
+		}
+		if(s.empty())
+		break;
+		
+		root=s.top();
+		s.pop();
+		cout<<root->data<< " ";
+		root=root->right;
+		
+	}
+	
+	
+}
+
+
+int main()
+{
+	struct TreeNode* newNode[7];
+	for(int i=0;i<7;i++)
+	{
+		newNode[i]=new TreeNode;
+	}
+	root=newNode[0];
+	
+	newNode[0]->data =9;
+    newNode[0]->left=newNode[1];
+    newNode[0]->right=newNode[2];
+    
+    newNode[1]->data =1;
+    newNode[1]->left=newNode[3];
+    newNode[1]->right=newNode[4];
+    
+    newNode[2]->data =2;
+    newNode[2]->left=newNode[5];
+    newNode[2]->right=newNode[6];
+    
+    newNode[3]->data =5;
+    newNode[3]->left=NULL;
+    newNode[3]->right=NULL;
+    
+    newNode[4]->data =3;
+    newNode[4]->left=NULL;
+    newNode[4]->right=NULL;
+    
+    newNode[5]->data =8;
+    newNode[5]->left=NULL;
+    newNode[5]->right=NULL;
+    
+    newNode[6]->data =7;
+    newNode[6]->left=NULL;
+    newNode[6]->right=NULL;
+	
+	cout<<"InOrder Traversal"<<endl;
+	inorder(root);
+	cout<<endl;
+	
+
+}
