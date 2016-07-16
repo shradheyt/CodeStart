969069b0f40a58ce1fc8e12a3576dd9762eb5ad6
shradheyt <shradhey029tripathi@gmail.com>
2016-07-17 03:56:17 +0530

    Recursive way to search in Binary Tree

---
+++ Trees/SearchInTree(Recursive).cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<queue>
+#include<limits.h>
+#include<queue>
+using namespace std;
+
+struct TreeNode
+{
+	int data;
+	struct TreeNode* left;
+	struct TreeNode* right;
+} *root=NULL;
+
+
+bool searchInTree(struct TreeNode* root,int data)
+{
+	if(root==NULL)
+	return false;
+	if(data==root->data)
+	return true;
+	
+	bool temp;
+	temp=searchInTree(root->left,data);
+	if(temp!=false)
+	return temp;
+	else
+	return searchInTree(root->right,data);
+	}
+
+
+
+int main()
+{
+	struct TreeNode* newNode[7];
+	for(int i=0;i<7;i++)
+	{
+		newNode[i]=new TreeNode;
+	}
+	root=newNode[0];
+	
+	newNode[0]->data =9;
+    newNode[0]->left=newNode[1];
+    newNode[0]->right=newNode[2];
+    
+    newNode[1]->data =1;
+    newNode[1]->left=newNode[3];
+    newNode[1]->right=newNode[4];
+    
+    newNode[2]->data =2;
+    newNode[2]->left=newNode[5];
+    newNode[2]->right=newNode[6];
+    
+    newNode[3]->data =5;
+    newNode[3]->left=NULL;
+    newNode[3]->right=NULL;
+    
+    newNode[4]->data =3;
+    newNode[4]->left=NULL;
+    newNode[4]->right=NULL;
+    
+    newNode[5]->data =8;
+    newNode[5]->left=NULL;
+    newNode[5]->right=NULL;
+    
+    newNode[6]->data =10;
+    newNode[6]->left=NULL;
+    newNode[6]->right=NULL;
+	
+	int n;
+	cin>>n;
+	cout<<"Search Result"<<endl;
+	cout<<searchInTree(root,n);
+	cout<<endl;
+	
+
+}
