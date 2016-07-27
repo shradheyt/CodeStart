04d0a47e7f6832467a22e8cab6bc2d86c65192f1
shradheyt <shradhey029tripathi@gmail.com>
2016-07-27 17:16:03 +0530

    Convert Tree to its Mirror

---
+++ Trees/convertToMirror.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+struct TreeNode
+{
+	int data;
+	TreeNode* left;
+	TreeNode* right;
+}*root=NULL;
+
+struct TreeNode* createNode(int data)
+{
+	struct TreeNode* newNode=new TreeNode;
+	newNode->data=data;
+	newNode->left=NULL;
+	newNode->right=NULL;
+	return newNode;
+}
+
+void insert(int data)
+{
+	struct TreeNode* newNode=createNode(data);
+	struct TreeNode* temp;
+	queue<TreeNode*> q;
+	if(!newNode)
+	cout<<"memory error";
+	if(root==NULL)
+	{
+		root=newNode;
+		return ;
+	}
+	
+	q.push(root);
+	while(!q.empty())
+	{
+		temp=q.front();
+		q.pop();
+		
+		if(temp->left)
+		q.push(temp->left);
+		else
+		{
+			temp->left=newNode;
+			return;
+		}
+		
+		if(temp->right)
+		q.push(temp->right);
+		else
+		{
+			temp->right=newNode;
+			return;
+		}
+	}
+}
+
+
+void print(int arr[],int len)
+{
+	for(int i=0;i<len;i++)
+	{
+		cout<<arr[len]<<" ";
+		
+	}
+	cout<<endl;
+}
+
+void convertToMirror(struct TreeNode* root)
+{
+	struct TreeNode* temp;
+	
+	if(root==NULL)
+	return;
+	convertToMirror(root->left);
+	convertToMirror(root->right);
+	
+	temp=root->left;
+	root->left=root->right;
+	root->right=temp;
+}
+
+
+void levelOrder(struct TreeNode* root)
+{
+	struct TreeNode* temp;
+	queue<TreeNode*> q;
+	
+	if(root==NULL)
+	return;
+	
+	q.push(root);
+	while(!q.empty())
+	{
+		temp=q.front();
+		q.pop();
+		
+		cout<< temp->data<< " ";
+		
+		if(temp->left)
+		q.push(temp->left);
+		
+		if(temp->right)
+		q.push(temp->right);
+	}	
+}
+
+
+int main()
+{
+	
+	insert(1);
+	insert(2);
+	insert(3);
+	insert(4);
+	insert(5);
+	insert(6);
+	insert(7);
+	levelOrder(root);
+	cout<<endl;
+	convertToMirror(root);
+	levelOrder(root);
+	
+	
+}
